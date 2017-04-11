0372bdb7c1ad73b774480043dc1a714673fd73d7
liucan <chuck7liu@gmail.com>
2017-04-11 23:12:10 +0800

    update 之前nginx配得不对。。

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const http = require('http');
-const Koa = require('koa');
-const app = new Koa();
-const cors = require('kcors');
+const http = require('http')
+const Koa = require('koa')
+const app = new Koa()
+const cors = require('kcors')
 app.use(cors({
     maxAge: 7 * 24 * 60 * 60
 }))
@@ -9,14 +9,14 @@ app.use(async (ctx, next)=> {
     ctx.body = 'hello world!'
 })
 const server = http.createServer(app.callback())
-const io = require('socket.io')(server);
+const io = require('socket.io')(server)
 io.on('connection', async (socket) => {
-    console.log(1)
-    socket.emit('news', { hello: 'world' });
+    console.log('新连接')
+    socket.emit('news', { hello: 'world' })
     socket.on('other event', async (data) => {
-        console.log(data);
-    });
-});
+        console.log(data)
+    })
+})
 server.listen(3000, async () => {
     console.log('server is running on port 3000')
-});
+})
