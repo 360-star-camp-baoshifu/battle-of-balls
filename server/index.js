a64e58dbb2635badc2ba03b8793c829a0a5232c9
GuoChen-WHU <496474070@qq.com>
2017-04-13 20:45:17 +0800

    User

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const http = require('http')
 const Koa = require('koa')
 const app = new Koa()
 const cors = require('kcors')
+let User = require('./model/User')
+
 app.use(cors({
     maxAge: 7 * 24 * 60 * 60
 }))
@@ -10,12 +12,29 @@ app.use(async (ctx, next)=> {
 })
 const server = http.createServer(app.callback())
 const io = require('socket.io')(server)
-io.on('connection', async (socket) => {
-    socket.emit('news', { hello: 'world' })
-    socket.on('other event', async (data) => {
-        console.log(data)
+
+io.on('connection', (socket) => {
+    console.log('新连接')
+    let user = new User(socket);
+
+    const fruitNum = User.num() === 1 ? 20 : 5;
+
+
+    io.emit('connected', User.getAllBalls());
+
+    socket.on('move', (data) => {
+    
+    })
+
+    socket.on('eatFood', data => {
+
+    })
+
+    socket.on('eatBall', data => {
+
     })
 })
+
 server.listen(3000, async () => {
     console.log('server is running on port 3000')
 })
