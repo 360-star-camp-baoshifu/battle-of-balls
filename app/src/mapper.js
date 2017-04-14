4bfd22810f16f007d80162e78d0a53808187d61a
yanyige <232218719@qq.com>
2017-04-14 09:39:35 +0800

    update

--- app/src/mapper.js
+++ app/src/mapper.js
@@ -1,9 +1,10 @@
 const defaultOptions = {
 	container: null,
-	MAP_WIDTH: 6000,
-	MAP_HEIGHT: 6000,
+	MAP_WIDTH: 3000,
+	MAP_HEIGHT: 3000,
 	USER_WIDTH: 600,
 	USER_HEIGHT: 600,
+	GRID: 100,
 };
 
 function drawFoods(cxt, color, x, y, r) {
@@ -12,6 +13,19 @@ function drawFoods(cxt, color, x, y, r) {
 	ctx.arc(x, y, r, 0, Math.PI * 2, true);
 	ctx.closePath();
 	ctx.fill();
+}
+
+function drawGrid(cxt, grid, width, height) {
+
+ 	for(let i = 0; i < this.width; i += 100) {
+ 		cxt.moveTo(i * grid, 0);//将画笔移到x0,y0处
+		cxt.lineTo(i * grid, height);//从x0,y0到x1,y1画一条线
+	}
+	for(let i = 0; i < this.height; i += 100) {
+ 		cxt.moveTo(0, i * grid);//将画笔移到x0,y0处
+		cxt.lineTo(width, i * grid;//从x0,y0到x1,y1画一条线
+	}
+
 }
 
 export default class Mapper{
@@ -23,9 +37,6 @@ export default class Mapper{
 		let {width, height} = options;
 		this.width = width, this.height = height;
 
-		if(options.render) {
-			this.render();
-		}
 	}
 
 	render(foods, user) {
@@ -41,6 +52,8 @@ export default class Mapper{
 		container.appendChild(mapCanvas);
 		container.appendChild(foodCanvas);
 
+
+
 		let mapCxt = mapCanvas.getContext('2d');
 		let foodCxt = foodCanvas.getContext('2d');
 
@@ -54,6 +67,8 @@ export default class Mapper{
 			top: user.x - MAP_HEIGHT / 2 + 'px',
 			left: user.y - MAP_WIDTH / 2 + 'px',
 		});
+
+
 	}
 
 	foodCanvasMove (user) {
